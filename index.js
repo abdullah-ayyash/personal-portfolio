49b7918db1bd71dfa411fd03bdc2d48979927bea
Abdullah Ayyash <abdullah392585@gmail.com>
2023-03-21 02:12:14 -0700

    add a contact page

--- index.js
+++ index.js
@@ -3,57 +3,102 @@ const previewEl = document.getElementById('preview');
 const closeBtn = document.getElementById('close-btn');
 const previewImg = document.getElementById('preview-img');
 const businessBtn = document.getElementById('business-btn');
-const myName = document.getElementById('my-name')
+const myName = document.getElementById('my-name');
+const logoImg = document.getElementById('logo-img');
+
+try {
+    chessBtn.addEventListener('click', () => {
+        previewImg.src = "./images/chess-preview.png"
+        previewEl.style.display = 'block';
+        scrollTo(0, 0);
+    })
+
+    businessBtn.addEventListener('click', () => {
+        previewImg.src = "./images/business-preview.png"
+        previewEl.style.display = 'block';
+        scrollTo(0, 0);
+    })
+
+    closeBtn.addEventListener('click', () => {
+        previewEl.style.display = 'none';
+        previewEl.style.transition = "all, 1s"
+    })
 
 
-chessBtn.addEventListener('click', ()=>{
-    previewImg.src = "./images/chess-preview.png"
-    previewEl.style.display = 'block';
-    scrollTo(0, 0);
-})
+    // Get the first letter of the heading text
+    const nameList = ['A', 'b', 'd', 'u', 'l', 'l', 'a', 'h', ' ', 'A', 'y', 'y', 'a', 's', 'h']
 
-businessBtn.addEventListener('click', ()=>{
-    previewImg.src = "./images/business-preview.png"
-    previewEl.style.display = 'block';
-    scrollTo(0, 0);
-})
-
-closeBtn.addEventListener('click', ()=>{
-    previewEl.style.display = 'none';
-    previewEl.style.transition = "all, 1s"
-})
+    let timeId = setInterval(frame, 200);
+    let index = 0;
 
 
-  // Get the first letter of the heading text
-const nameList = ['A','b','d','u','l','l','a','h',' ', 'A','y','y','a','s','h']
+    function frame() {
+        if (index < nameList.length) {
+            myName.textContent += nameList[index];
+            index++;
+        }
 
-let timeId = setInterval(frame,200);
-let index = 0;
 
+        if (index === nameList.length) {
+            clearInterval(timeId);
+            clearInterval(timeId2);
+            myName.style.borderRight = '';
+        }
 
-function frame(){
-    if(index < nameList.length){
-        myName.textContent += nameList[index];
-        index++;
     }
-    
-    
-    if(index === nameList.length){
-        clearInterval(timeId);
-        clearInterval(timeId2);
-        myName.style.borderRight = '';
+
+    let timeId2 = setInterval(typingEff, 200);
+
+    function typingEff() {
+        let borderVal = myName.style.borderRight;
+        if (borderVal === '3px solid springgreen') {
+            myName.style.borderRight = '';
+        }
+        else {
+            myName.style.borderRight = '3px solid springgreen';
+        }
     }
-    
+
+    let newImagePreview = document.getElementById('preview-img');
+    previewImg.onclick = () => {
+        if (newImagePreview.style.scale !== '2') {
+            newImagePreview.style.scale = '2';
+            newImagePreview.style.cursor = 'zoom-out'
+        }
+        else {
+            newImagePreview.style.scale = '1';
+            newImagePreview.style.cursor = 'zoom-in'
+        }
+    }
+}
+catch (error) {
+    console.log(error);
+}
+let timeId3 = setInterval(rotateImg, 20);
+
+let deg = 0;
+function rotateImg() {
+    if (deg > 360) {
+        clearInterval(timeId3);
+    }
+    else {
+        logoImg.style.rotate = deg + "deg"
+        deg += 2;
+    }
+
 }
 
-let timeId2 = setInterval(typingEff, 200);
 
-function typingEff(){
-    let borderVal = myName.style.borderRight;
-    if(borderVal === '3px solid springgreen'){
-        myName.style.borderRight = '';
+let timeId4 = setInterval(contactStat, 150);
+
+let contactHeading = document.getElementById('contact-h1');
+let val = 0;
+function contactStat() {
+    if (val !== 1) {
+        contactHeading.style.opacity = `${val}`
+        val += 0.1;
     }
     else{
-        myName.style.borderRight = '3px solid springgreen';
+        contactHeading.style.borderBottom = 'none'
     }
 }
